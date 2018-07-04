8486d7218f3c6f3da3c9db3cf5c38b1220734c04
Pochobradský Pavel <pochobradsky@komix.com>
2018-07-04 14:00:57 +0200

    Added user widget

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import './index.css';
 import React from 'react';
+import UserWidget from '../UserWidget';
 
-const Header = ({titleColor}) => {
+const Header = ({titleColor, showUserWidget}) => {
     var className = titleColor == "black" ? "siteTitleSignedIn" : "siteTitle";
     var wrapClassName = titleColor == "black" ? "siteHeaderSignedIn" : "siteHeader";
+    const userWidget = window.userEmail != undefined ? <UserWidget /> : null;
     return (
         <div className={wrapClassName}>
             <p className={className}>E.</p>
+            {userWidget}
         </div>
     )
 }
--- src/components/UserWidget/index.js
+++ src/components/UserWidget/index.js
@@ -2,20 +2,49 @@ import React, { Component } from "react";
 import "./index.css";
 import { Link } from 'react-router-dom';
 
-export default class UserWidget extends Component {
+class UserWidget extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      open: false,
+      width: 0
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      width: window.getComputedStyle(document.getElementsByClassName("userWidget")[0]).getPropertyValue("width")
+    });
+  }
+
+  open = () => {
+    this.setState((prevState) => {
+      return {open: !prevState.open}
+    })
   }
 
   render() {
 
+    const wrapClass = this.state.open ? "userWidget openUserWidget" : "userWidget";
+
+    if (this.state.width > 0) {
+      document.getElementsByClassName("userWidget")[0].style.maxWidth = this.state.width;
+    }
+    const logoutButton = this.state.open ? <button className="logoutButton" style={{maxWidth: this.state.width}}>Logout</button> : null;
+
     return (
-    	<div className="userWidget">
-    		<p className="welcomeText">Hi there!</p>
-    		<p className="userEmail">{window.userEmail}</p>
-    		<Link className="logoutButton" to="/Login">Log out</Link>
-    	</div>
-    );
+      <div className={wrapClass}>
+        <div className="userCircle">
+          <p>TW</p>
+        </div>
+        <p>Tom Watts</p>
+        <button className="userDropdown" onClick={this.open}><i className="material-icons">arrow_drop_down</i></button>
+        {logoutButton}
+      </div>
+    ) 
   }
 }
+
+export default UserWidget;
